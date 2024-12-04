a052180296adfb5cdb8fc6b02b8b9f1132f72537
Trayana Boykova <trayana.boykova@gmail.com>
2024-12-04 12:00:40 +0200

    Update functions.php

---
+++ wp-content/themes/lifestyle-blog/functions.php
@@ -0,0 +1,5 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'lifestyle_blog_enqueue_assets' );
+function lifestyle_blog_enqueue_assets() {
+    wp_enqueue_style( 'lifestyle-blog-style', get_stylesheet_uri() );
+}
