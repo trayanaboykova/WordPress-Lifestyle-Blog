3f58943553a5bb0cc5b0aaa319dac22fe1725677
Trayana Boykova <trayana.boykova@gmail.com>
2024-12-05 12:30:28 +0200

    delete theme
    starting over again, mixed up files

--- wp-content/themes/lifestyle-blog-theme/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_theme_support('title-tag');
-add_theme_support( 'post-thumbnails' );
-
-add_action('wp_enqueue_scripts', 'lifestyle_blog_enqueue_assets');
-function lifestyle_blog_enqueue_assets() {
-    wp_enqueue_style('lifestyle-blog', get_stylesheet_uri());
-}
