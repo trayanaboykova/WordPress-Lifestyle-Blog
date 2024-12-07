961c3d5f9e87a20d6a12b308a20fc393f7c4cf77
Trayana Boykova <trayana.boykova@gmail.com>
2024-12-08 01:38:47 +0200

    Update functions.php
    adding pagination in functions.php

--- wp-content/themes/lifestyle-blog/functions.php
+++ wp-content/themes/lifestyle-blog/functions.php
@@ -5,9 +5,25 @@ add_theme_support( 'post-thumbnails' );
 
 add_action( 'wp_enqueue_scripts', 'lifestyle_theme_enqueue_styles' );
 function lifestyle_theme_enqueue_styles() {
-	// Load all CSS files
-	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/css/styles.css');
-	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/css/vendor.css');
+	// Enqueue vendor styles first (since other styles depend on it)
+	wp_enqueue_style( 'lifestyle-vendor-styles', get_stylesheet_directory_uri() . '/css/vendor.css' );
+
+	// Enqueue custom styles with 'vendor.css' as a dependency
+	wp_enqueue_style( 'lifestyle-theme-styles', get_stylesheet_directory_uri() . '/css/styles.css', array( 'lifestyle-vendor-styles' ) );
+
+	// Enqueue main theme styles (if applicable)
+	wp_enqueue_style( 'lifestyle-main-style', get_template_directory_uri() . '/style.css', array( 'lifestyle-vendor-styles' ) );
 }
+
+//Pagination
+function fix_paged_parameter($query) {
+	if (!is_admin() && $query->is_main_query()) {
+		if (is_category() || is_tag() || is_archive()) {
+			if (get_query_var('paged')) {
+				$query->set('paged', get_query_var('paged'));
+			}
+		}
+	}
+}
+add_action('pre_get_posts', 'fix_paged_parameter');
 
